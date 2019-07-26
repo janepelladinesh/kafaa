718f1f6f245b545514e5df91f52f9711d347bae6
Dinesh <janepelladinesh97@gmail.com>
2019-07-26 13:54:38 +0530

    Update index.py

--- kafka/producer/index.py
+++ kafka/producer/index.py
@@ -5,7 +5,7 @@ import sys
 
 from kafka import  KafkaProducer
 
-ip=sys.argv[1]
+server=sys.argv[1]
 
 workers = int(os.environ.get('GUNICORN_PROCESSES', '3'))
 threads = int(os.environ.get('GUNICORN_THREADS', '1'))
@@ -17,7 +17,7 @@ application = Flask(__name__)
 @application.route("/")
 def hello():
     
-    producer = KafkaProducer(bootstrap_servers=ip)
+    producer = KafkaProducer(bootstrap_servers=server)
     producer.send('test', b"producer message-hello")
     time.sleep(10)
   
