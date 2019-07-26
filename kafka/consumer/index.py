0e3addb2e7ce1a862e6de83643036565eed3743c
Dinesh <janepelladinesh97@gmail.com>
2019-07-26 13:52:32 +0530

    Update index.py

--- kafka/consumer/index.py
+++ kafka/consumer/index.py
@@ -3,7 +3,7 @@ from flask import Flask
 import sys
 from kafka import KafkaConsumer
 
-ip=sys.argv[1]
+server=sys.argv[1]
 
 workers = int(os.environ.get('GUNICORN_PROCESSES', '3'))
 threads = int(os.environ.get('GUNICORN_THREADS', '1'))
@@ -14,23 +14,24 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    consumer = KafkaConsumer(bootstrap_servers=ip,
+    consumer = KafkaConsumer(bootstrap_servers=server,
                              auto_offset_reset='earliest',
                              consumer_timeout_ms=1000)
     consumer.subscribe(['test'])
 
-    k="old messages:\n"
-    a="new messages:\n"
-    b=""
+    old_messages="old_messages:\n"
+    new_messages="new_messages:\n"
+    last_message="\n"
     for message in consumer:
         print(message.value)
-        k+=message.value+"\n"
-        b=message.value
-    a+=b
-    c=k+"\n"+a
+        print '\n'
+        old_messages+=message.value
+        last_message=message.value
+    new_messages="new_messages:\n"+last_message
+    return_values=old_messages+"\n"+new_messages
 
 
-    return c
+    return return_values
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0', port=8080)
